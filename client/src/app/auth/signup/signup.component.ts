ce190feda19a20edab0f03ba60e48d82662caa1d
Siam Ahmed <ahmedsiam397793@gmail.com>
2022-05-28 12:04:50 +0600

    client validation added and connected with server

--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { catchError } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -10,7 +11,9 @@ import { AuthService } from '../auth.service';
 export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
-  passwordMismatch: boolean;
+  displayError: boolean = false;
+  color;
+  alert;
   constructor(
     private _formBuilder: FormBuilder,
     private _authService: AuthService
@@ -20,24 +23,66 @@ export class SignupComponent implements OnInit {
     this.signupForm = this._formBuilder.group({
       username: ['', Validators.required],
       email: ['', [Validators.email, Validators.required]],
-      dateOfBirth: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
+      dateOfBirth: ['', [Validators.required, this.ageValidator]],
+      password: ['', [Validators.required, Validators.pattern(
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+      ),
+      Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, this.confirmPasswordValidator]],
+
     })
+  }
+
+  findStrengthOfPassword() {
+    if (this.signupForm.controls['password']) {
+      let password: string = this.signupForm.controls['password'].value
+
+    }
   }
 
   signup() {
     const formValues = this.signupForm.getRawValue()
-    if (formValues.confirmPassword === formValues.password) {
-      this.passwordMismatch = false;
-      this._authService.signup(formValues).subscribe(res => {
-        console.log(res);
 
-      })
-    } else {
-      this.passwordMismatch = true;
-      console.log(this.passwordMismatch);
+    this._authService.signup(formValues).subscribe(
+      {
+        next: (res) => this.showAlert('Signup Sucessful!', '#0de057'),
+        error: (err) => this.showAlert('Signup unsuccessful!', '#e60d00')
+      }
+    )
+  }
+
+  sexHaver() {
+    console.log(this.signupForm.controls['password']);
+
+  }
+
+  private ageValidator(control) {
+    if (control.value && new Date().getFullYear() - new Date(control.value).getFullYear() < 18) {
+      return { underage: true }
     }
+    return null;
+  }
+
+  private confirmPasswordValidator(control: FormGroup) {
+    if (control.value) {
+      let password = control.parent.controls['password'].value;
+      let confirmPassword = control.value
+      if (password !== confirmPassword) {
+        return { passwordMismatch: true }
+      }
+    }
+    return null
+
+  }
+
+  showAlert(message, color) {
+    this.alert = {
+      message,
+      color
+    }
+    setTimeout(() => {
+      this.alert = null
+    }, 3000);
   }
 
 }
--- server/Services/UserService.cs
+++ server/Services/UserService.cs
@@ -2,6 +2,8 @@
 using MongoDB.Driver;
 using server.Interface;
 using server.Model;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace server.Services
 {
@@ -22,7 +24,40 @@ namespace server.Services
 
         public async Task signupUser(User user)
         {
-            await _userCollection.InsertOneAsync(user);
+            string? hashedPassword;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                hashedPassword = GetHash(sha256Hash, user?.Password);
+            }
+                User newUser = new User()
+            {
+                Username = user.Username,
+                Password = hashedPassword,
+                Email = user.Email,
+                DateOfBirth = user.DateOfBirth
+            };
+            await _userCollection.InsertOneAsync(newUser);
+        }
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
         }
     }
 }
