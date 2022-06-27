e4f82f04445dadfceb6d08facd16ea844af47bdb
Siam Ahmed <ahmedsiam397793@gmail.com>
2022-06-28 03:34:13 +0600

    fluent validation dependency fixed

--- server/Program.cs
+++ server/Program.cs
@@ -11,8 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers()
-    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<SignupRequestDtoValidator>())
-    .AddFluentValidation(fv=>fv.RegisterValidatorsFromAssemblyContaining<LoginRequestDtoValidator>());
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Program>());
 
 builder.Services.Configure<UserDatabaseConfig>(
     builder.Configuration.GetSection("DatabaseConfig"));
