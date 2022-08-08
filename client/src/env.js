58ba9c85ccd34a2fe94daf9032870e518c809a6d
Tahsin Tunan <tahsintunan@gmail.com>
2022-08-08 17:58:24 +0600

    Client-side code removed
    Signed-off-by: Tahsin Tunan <tahsintunan@gmail.com>

--- client/src/env.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  window.__env = window.__env || {};
-
-  // API url
-  window.__env.apiUrl = 'http://localhost:5001/';
-
-  // Whether or not to enable debug mode
-  // Setting this to false will disable console output
-}(this));
