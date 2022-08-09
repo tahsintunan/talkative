e137db05c61ac98aaf5891677baabde46afbc61f
Tahsin Tunan <tahsintunan@gmail.com>
2022-08-09 14:06:11 +0600

    Rabbitmq Service implemented
    Signed-off-by: Tahsin Tunan <tahsintunan@gmail.com>

---
+++ server/Dto/MessageDto/MessageDto.cs
@@ -0,0 +1,8 @@
+namespace server.Dto.MessageDto;
+
+public class MessageDto
+{
+    public string? SenderId { get; set; }
+    public string? ReceiverId { get; set; }
+    public string? MessageText { get; set; }
+}
