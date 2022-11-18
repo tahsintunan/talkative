c3d46e386dcf586de55e013a05679cd2c5d24274
Siam Ahmed <ahmedsiam397793@gmail.com>
2022-11-18 06:34:33 +0600

    unit test to new project

--- UnitTests/BlockFilterTests.cs
+++ UnitTests/BlockFilterTests.cs
@@ -1,4 +1,4 @@
-using Application.Common.Class;
+﻿using Application.Common.Class;
 using Application.Common.Interface;
 using Application.Common.ViewModels;
 using FakeItEasy;
@@ -11,9 +11,24 @@ public class BlockFilterTest
     public static IEnumerable<object[]> IsBlocked_ShouldReturnCorrectBool_Data =>
         new List<object[]>
         {
-            new object[] { new Blockable { UserId = "1" }, new HashSet<string>(), false },
-            new object[] { new Blockable { UserId = "1" }, new HashSet<string> { "2", "3", "4" }, false },
-            new object[] { new Blockable { UserId = "3" }, new HashSet<string> { "2", "3", "4" }, true }
+            new object[]
+            {
+                new Blockable { UserId = "1" },
+                new HashSet<string>(),
+                false
+            },
+            new object[]
+            {
+                new Blockable { UserId = "1" },
+                new HashSet<string> { "2", "3", "4" },
+                false
+            },
+            new object[]
+            {
+                new Blockable { UserId = "3" },
+                new HashSet<string> { "2", "3", "4" },
+                true
+            }
         };
 
     public static IEnumerable<object[]> GetBlockedUserIds_ShouldReturnUserIdsOfBlockedUsers_Data =>
@@ -21,27 +36,31 @@ public class BlockFilterTest
         {
             new object[]
             {
-                new List<string> { "1", "2" }, new List<string> { "3", "4" }, false,
+                new List<string> { "1", "2" },
+                new List<string> { "3", "4" },
+                false,
                 new HashSet<string> { "1", "2", "3", "4" }
             },
             new object[]
             {
-                null!, new List<string> { "3", "4" }, false,
+                null!,
+                new List<string> { "3", "4" },
+                false,
                 new HashSet<string> { "3", "4" }
             },
             new object[]
             {
-                new List<string> { "1", "2" }, null!, false,
+                new List<string> { "1", "2" },
+                null!,
+                false,
                 new HashSet<string> { "1", "2" }
             },
+            new object[] { new List<string>(), new List<string>(), false, new HashSet<string>() },
             new object[]
             {
-                new List<string>(), new List<string>(), false,
-                new HashSet<string>()
-            },
-            new object[]
-            {
-                new List<string> { "1", "2" }, new List<string> { "3", "4" }, true,
+                new List<string> { "1", "2" },
+                new List<string> { "3", "4" },
+                true,
                 new HashSet<string>()
             }
         };
@@ -51,7 +70,8 @@ public class BlockFilterTest
     public void IsBlocked_ShouldReturnCorrectBool(
         Blockable blockable,
         IReadOnlySet<string> blockedUserIds,
-        bool expected)
+        bool expected
+    )
     {
         // Arrange
         var userService = A.Fake<IUser>();
@@ -64,14 +84,14 @@ public class BlockFilterTest
         Assert.Equal(expected, actual);
     }
 
-
     [Theory]
     [MemberData(nameof(GetBlockedUserIds_ShouldReturnUserIdsOfBlockedUsers_Data))]
     public async void GetBlockedUserIds_ShouldReturnUserIdsOfBlockedUsers(
         IList<string> blocked,
         IList<string> blockedBy,
         bool isAdmin,
-        HashSet<string> expected)
+        HashSet<string> expected
+    )
     {
         // Arrange
         var userService = A.Fake<IUser>();
